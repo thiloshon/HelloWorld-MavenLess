fdf5de8a948f231da133b3aa3a1307c830dbfe0a
thiloshon <thiloshon@gmail.com>
2018-02-20 12:06:45 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Main {
+    public static void main(String[] args) throws JSONException {
+
+        String HelloWorld = "Hello World";
+
+        JSONObject HelloWorldJSON = new JSONObject().put("Message", HelloWorld);
+
+        System.out.println(HelloWorldJSON.toString());
+    }
+}
